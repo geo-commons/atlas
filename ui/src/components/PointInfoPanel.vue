de0e89172667014da9dd0b249d2ce620e6b9ecdc
Fabian Buijing <fabianbuijing@gmail.com>
2021-03-10 10:37:11 +0100

    improve close sidepanel experience

--- ui/src/components/PointInfoPanel.vue
+++ ui/src/components/PointInfoPanel.vue
@@ -6,11 +6,10 @@
 
     <SidePanel :showPanel="showInfoPanel && showSidePanel" @toggle-side-panel="toggleSidePanel">
       <template v-slot:search>
-        <Search :show-border="true" @on-close="closeInfoPanel">
-          <template v-slot:default>
-            <input type="text" name="search" placeholder="Zoek adres" autocomplete="off" />
-          </template>
-        </Search>
+        <button class="iconbutton back-button" type="button" v-tippy='{ placement : "right" }' content="Ga terug" aria-label="Ga terug" @click="closeInfoPanel">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"/></svg>
+        </button>
+        <h1>"coordinaten/adres"</h1>
       </template>
 
       <template v-slot:default>
@@ -23,14 +22,12 @@
 <script>
 import SidePanel from './SidePanel'
 import FeatureInfo from './FeatureInfo'
-import Search from './Search'
 
 export default {
   name: 'MorePanel',
   components: {
     SidePanel,
     FeatureInfo,
-    Search
   },
   methods: {
     closeInfoPanel() {
@@ -65,5 +62,16 @@ export default {
   border-top-right-radius: var(--radius-small);
   border-bottom-right-radius: var(--radius-small);
   box-shadow: var(--shadow-normal);
+}
+
+h1 {
+  font-size: var(--font-size-normal);
+}
+
+.back-button {
+  width: 40px;
+  height: 40px;
+  border-radius: var(--radius-normal);
+  border: 1px solid var(--color-grey-60);
 }
 </style>
--- ui/src/components/Search.vue
+++ ui/src/components/Search.vue
@@ -4,8 +4,8 @@
         <button v-if="this.$listeners['show-data-panel']" class="iconbutton toggle-button" type="button" v-tippy='{ placement : "bottom" }' content="Toon data" aria-label="Toon data" @click="showDataPanel">
           <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><rect fill="none" height="24" width="24"/><path fill="currentColor" d="M14.17,5L19,9.83V19H5V5L14.17,5L14.17,5 M14.17,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9.83 c0-0.53-0.21-1.04-0.59-1.41l-4.83-4.83C15.21,3.21,14.7,3,14.17,3L14.17,3z M7,15h10v2H7V15z M7,11h10v2H7V11z M7,7h7v2H7V7z"/></g></svg>
         </button>
-        <button v-if="this.$listeners['on-close']" class="iconbutton toggle-button" type="button" v-tippy content="Sluit paneel" aria-label="Sluit paneel" @click="onClose">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
+        <button v-if="this.$listeners['on-close']" class="iconbutton toggle-button" type="button" v-tippy content="Ga terug" aria-label="Ga terug" @click="onClose">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"/></svg>
         </button>
 
         <slot></slot>
