756c95690594b1852a09d1aaf2df774b19a1272d
Bart Jeukendrup <bart@jeukendrup.nl>
2021-04-27 17:55:58 +0200

    feat: add basic wmts capabilities for nationaalgeoregister

--- ui/src/utils/controller.js
+++ ui/src/utils/controller.js
@@ -2,7 +2,25 @@ import Map from 'ol/Map'
 import View from 'ol/View'
 import TileLayer from 'ol/layer/Tile'
 import TileWMS from 'ol/source/TileWMS'
+import WMTSSource from 'ol/source/WMTS'
+import WMTSTileGrid from 'ol/tilegrid/WMTS'
+import Projection from 'ol/proj/Projection'
+import { getTopLeft } from 'ol/extent.js'
+
 import { defaults as defaultInteractions, DragRotateAndZoom } from 'ol/interaction'
+
+const rdProjection = new Projection({
+  code: 'EPSG:28992',
+  extent: [-285401.92, 22598.08, 595401.92, 903401.92]
+})
+
+// can be calculated based on resolution z0, written out for clarity
+// see https://www.geonovum.nl/uploads/standards/downloads/nederlandse_richtlijn_tiling_-_versie_1.1.pdf
+const resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210]
+const matrixIds = new Array(15)
+for (var i = 0; i < 15; ++i) {
+  matrixIds[i] = i
+}
 
 class MapController {
   constructor(settings) {
@@ -17,9 +35,29 @@ class MapController {
   }
 
   addLayer(layer) {
-    this.layers = [
-      ...this.layers,
-      new TileLayer({
+    let tileLayer
+    if (layer.source === 'wmts') {
+      tileLayer = new TileLayer({
+        id: layer.id,
+        visible: (layer.is_visible === true),
+        layerName: layer.name,
+        opacity: layer.opacity,
+        extent: rdProjection.extent,
+        source: new WMTSSource({
+          url: layer.url,
+          layer: layer.name,
+          matrixSet: 'EPSG:28992',
+          format: 'image/png',
+          projection: rdProjection,
+          tileGrid: new WMTSTileGrid({
+            origin: getTopLeft(rdProjection.getExtent()),
+            resolutions,
+            matrixIds
+          })
+        })
+      })
+    } else {
+      tileLayer = new TileLayer({
         id: layer.id,
         visible: (layer.is_visible === true) || this.settings.visibleLayers.includes(layer.id),
         layerName: layer.name,
@@ -28,11 +66,14 @@ class MapController {
           projection: 'EPSG:28992',
           url: layer.url,
           servertype: layer.server_type,
-          params: {
-            'layers': layer.name
-          },
+          params: { 'layers': layer.name },
         })
       })
+    }
+
+    this.layers = [
+      ...this.layers,
+      tileLayer
     ]
   }
 
