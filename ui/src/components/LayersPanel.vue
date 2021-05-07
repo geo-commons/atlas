5d3f75da61339b4c61dccdfc3e053e105c0b767a
Fabian <fabianbuijing@gmail.com>
2021-05-07 09:53:40 +0000

    add focus outline #127

--- ui/src/components/LayersPanel.vue
+++ ui/src/components/LayersPanel.vue
@@ -1,32 +1,102 @@
 <template>
   <div>
     <div class="buttons-wrapper">
-      <div class="buttons" :class="{ isOpen: this.panel === 'layers' || this.panel === 'activeLayers', showVisibleLayers: visibleLayers.length > 0 }">
-        <button class="iconbutton" :class="{ isActive: this.panel === 'layers' }" @click="() => togglePanel('layers')" v-tippy content="Alle lagen" aria-label="Toon alle lagen" :aria-expanded="String(this.panel === 'layers')" aria-controls="layers">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="currentColor" d="M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16zm0-11.47L17.74 9 12 13.47 6.26 9 12 4.53z"/></svg>
+      <div
+        class="buttons"
+        :class="{
+          isOpen: this.panel === 'layers' || this.panel === 'activeLayers',
+          showVisibleLayers: visibleLayers.length > 0,
+        }"
+      >
+        <button
+          class="iconbutton"
+          :class="{ isActive: this.panel === 'layers' }"
+          @click="() => togglePanel('layers')"
+          v-tippy
+          content="Alle lagen"
+          aria-label="Toon alle lagen"
+          :aria-expanded="String(this.panel === 'layers')"
+          aria-controls="layers"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 0 24 24"
+            width="24"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              fill="currentColor"
+              d="M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16zm0-11.47L17.74 9 12 13.47 6.26 9 12 4.53z"
+            />
+          </svg>
         </button>
 
-        <button class="iconbutton" :class="{ isActive: this.panel === 'activeLayers' }" @click="() => togglePanel('activeLayers')" v-tippy content="Zichtbare lagen" aria-label="Toon zichtbare lagen" :aria-expanded="String(this.panel === 'activeLayers')" aria-controls="visibleLayers">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="currentColor" d="M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z"/></svg>
+        <button
+          class="iconbutton"
+          :tabindex="visibleLayers.length > 0 ? 0 : -1"
+          :class="{ isActive: this.panel === 'activeLayers' }"
+          @click="() => togglePanel('activeLayers')"
+          v-tippy
+          content="Zichtbare lagen"
+          aria-label="Toon zichtbare lagen"
+          :aria-expanded="String(this.panel === 'activeLayers')"
+          aria-controls="visibleLayers"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 0 24 24"
+            width="24"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              fill="currentColor"
+              d="M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z"
+            />
+          </svg>
         </button>
       </div>
 
       <transition name="fade">
-        <div class="counter visible-layer-counter" v-if="visibleLayers.length > 0">{{ visibleLayers.length }}</div>
+        <div
+          class="counter visible-layer-counter"
+          v-if="visibleLayers.length > 0"
+        >
+          {{ visibleLayers.length }}
+        </div>
       </transition>
     </div>
 
     <transition name="fade">
       <ul class="layers" v-if="this.panel === 'layers'" id="layers">
         <li>
-          <ExpandButton v-for="category in categories" :key="category.id" :title="category.title" class="category-wrapper">
+          <ExpandButton
+            v-for="category in categories"
+            :key="category.id"
+            :title="category.title"
+            class="category-wrapper"
+          >
             <template v-slot:button>
-              <div v-if="category.layers.filter((layer) => layer.is_visible).length > 0" class="counter layer-counter">{{category.layers.filter((layer) => layer.is_visible).length}}</div>
+              <div
+                v-if="
+                  category.layers.filter((layer) => layer.is_visible).length > 0
+                "
+                class="counter layer-counter"
+              >
+                {{ category.layers.filter((layer) => layer.is_visible).length }}
+              </div>
             </template>
             <template v-slot:default>
               <ul :id="category.id" class="sublayers">
                 <li v-for="layer in category.layers" v-bind:key="layer.id">
-                  <input type="checkbox" :name="layer.id" :id="layer.id" :checked="layer.is_visible" @change="() => onSelectLayer(layer)" />
+                  <input
+                    type="checkbox"
+                    :name="layer.id"
+                    :id="layer.id"
+                    :checked="layer.is_visible"
+                    @change="() => onSelectLayer(layer)"
+                  />
                   <label :for="layer.id">{{ layer.title }}</label>
                 </li>
               </ul>
@@ -37,73 +107,85 @@
     </transition>
 
     <transition name="fade">
-      <ul v-if="visibleLayers.length > 0 && this.panel === 'activeLayers'" class="visible-layers" id="visibleLayers">
-          <VisibleLayer v-for="layer in visibleLayers" v-bind:key="layer.id" :layer="layer" @set-layer-opacity="setLayerOpacity" @toggle-layer="onSelectLayer" />
-        </ul>
+      <ul
+        v-if="visibleLayers.length > 0 && this.panel === 'activeLayers'"
+        class="visible-layers"
+        id="visibleLayers"
+      >
+        <VisibleLayer
+          v-for="layer in visibleLayers"
+          v-bind:key="layer.id"
+          :layer="layer"
+          @set-layer-opacity="setLayerOpacity"
+          @toggle-layer="onSelectLayer"
+        />
+      </ul>
     </transition>
   </div>
 </template>
 
 <script>
-import ExpandButton from './ExpandButton'
-import VisibleLayer from './VisibleLayer'
+import ExpandButton from "./ExpandButton";
+import VisibleLayer from "./VisibleLayer";
 
 export default {
-  name: 'LayersPanel',
+  name: "LayersPanel",
   components: {
     ExpandButton,
-    VisibleLayer
+    VisibleLayer,
   },
   data() {
     return {
-      panel: ''
-    }
+      panel: "",
+    };
   },
   methods: {
     togglePanel(selectedPanel) {
-      this.panel = selectedPanel !== this.panel ? selectedPanel : ''
+      this.panel = selectedPanel !== this.panel ? selectedPanel : "";
     },
     onSelectLayer(selectedLayer) {
-      this.$emit('toggle-layer', [ selectedLayer.id, !selectedLayer.is_visible ])
+      this.$emit("toggle-layer", [selectedLayer.id, !selectedLayer.is_visible]);
     },
     setLayerOpacity(values) {
-      this.$emit('set-layer-opacity', values)
-    }
+      this.$emit("set-layer-opacity", values);
+    },
   },
   computed: {
     categories() {
-      let categories = []
+      let categories = [];
 
       this.layers.forEach((layer) => {
         if (!layer.category) {
-          return
+          return;
         }
 
-        const existingCategory = categories.find((c) => c.id === layer.category.id)
+        const existingCategory = categories.find(
+          (c) => c.id === layer.category.id
+        );
 
         if (existingCategory) {
-          existingCategory.layers.push(layer)
-          return
+          existingCategory.layers.push(layer);
+          return;
         }
 
         const newCategory = {
           ...layer.category,
-          layers: [layer]
-        }
+          layers: [layer],
+        };
 
-        categories.push(newCategory)
-      })
+        categories.push(newCategory);
+      });
 
-      return categories
+      return categories;
     },
     visibleLayers() {
-      return this.layers.filter((layer) => layer.category && layer.is_visible)
-    }
+      return this.layers.filter((layer) => layer.category && layer.is_visible);
+    },
   },
   props: {
     layers: Array,
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
@@ -118,7 +200,7 @@ export default {
   overflow: hidden;
   border-radius: var(--radius-normal);
   box-shadow: var(--shadow-normal);
-  transition: width .1s ease, border-radius .1s;
+  transition: width 0.1s ease, border-radius 0.1s;
 }
 
 .buttons.isOpen {
@@ -127,7 +209,7 @@ export default {
 }
 
 .buttons.showVisibleLayers {
-    width: calc(var(--width-button-large) * 2 + 1px);
+  width: calc(var(--width-button-large) * 2 + 1px);
 }
 
 .iconbutton {
@@ -152,9 +234,13 @@ export default {
   position: absolute;
   bottom: var(--width-button-large);
   left: 0;
-  max-height: calc(100vh - ((var(--width-button-large) * 2) + (var(--padding-screen) * 4)));
+  max-height: calc(
+    100vh - ((var(--width-button-large) * 2) + (var(--padding-screen) * 4))
+  );
   width: calc(var(--width-detail) - (var(--padding-screen) * 2));
-  max-width: calc(100vw - (var(--padding-screen) * 3) - var(--width-button-normal));
+  max-width: calc(
+    100vw - (var(--padding-screen) * 3) - var(--width-button-normal)
+  );
   overflow-y: auto;
   background: white;
   border-radius: var(--radius-small);
@@ -165,12 +251,17 @@ export default {
 @media (min-width: 576px) {
   .showInfoPanel .layers,
   .showInfoPanel .visible-layers {
-    max-width: calc(100vw - (var(--padding-screen) * 3) - var(--width-button-normal) - var(--width-detail));
+    max-width: calc(
+      100vw - (var(--padding-screen) * 3) - var(--width-button-normal) -
+        var(--width-detail)
+    );
   }
 
   .showDataPanel .layers,
   .showDataPanel .visible-layers {
-    max-width: calc(50vw - (var(--padding-screen) * 3) - var(--width-button-normal));
+    max-width: calc(
+      50vw - (var(--padding-screen) * 3) - var(--width-button-normal)
+    );
   }
 }
 
