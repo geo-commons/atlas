b2a8d1b525f83e62903dac33d1dd9f19eb5a2ff7
Fabian Buijing <fabianbuijing@gmail.com>
2021-05-12 10:22:33 +0200

    style fancy link

--- ui/src/components/FeatureInfo.vue
+++ ui/src/components/FeatureInfo.vue
@@ -2,7 +2,7 @@
   <ExpandButton v-if="feature" :title="''" :isOpen="isOpen" class="feature">
     <img v-if="feature.properties && feature.properties.featuredImage" :src="feature.properties.featuredImage" class="featured-image" />
     <div class="content">
-    <div v-if="feature.properties" class="special-properties">
+    <div v-if="feature.properties" class="properties">
       <h1 v-if="feature.properties.title">{{ feature.properties.title }}</h1>
       <p v-if="feature.properties.description" class="markdown">
         <vue-simple-markdown :source="feature.properties.description" />
@@ -25,10 +25,11 @@
         />
       </div>
     </div>
-    <div v-if="feature.properties" class="special-properties">
+    <div v-if="feature.properties">
       <div v-if="feature.properties.link" class="link">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px"  height="24px" fill="#4285F4"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"/></svg>
+        <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="public_black_24dp" transform="translate(-2.000000, -2.000000)" fill="currentColor" fill-rule="nonzero"> <path d="M12,2 C6.48,2 2,6.48 2,12 C2,17.52 6.48,22 12,22 C17.52,22 22,17.52 22,12 C22,6.48 17.52,2 12,2 Z M4,12 C4,11.39 4.08,10.79 4.21,10.22 L8.99,15 L8.99,16 C8.99,17.1 9.89,18 10.99,18 L10.99,19.93 C7.06,19.43 4,16.07 4,12 Z M17.89,17.4 C17.63,16.59 16.89,16 15.99,16 L14.99,16 L14.99,13 C14.99,12.45 14.54,12 13.99,12 L7.99,12 L7.99,10 L9.99,10 C10.54,10 10.99,9.55 10.99,9 L10.99,7 L12.99,7 C14.09,7 14.99,6.1 14.99,5 L14.99,4.59 C17.92,5.77 20,8.65 20,12 C20,14.08 19.19,15.98 17.89,17.4 Z" id="Shape"></path> </g> </g> </svg>
         <vue-simple-markdown :source="feature.properties.link" class="markdown" />
+        <svg width="14px" height="14px" viewBox="0 0 14 14" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="open_in_new_black_18dp-(1)" transform="translate(-2.000000, -2.000000)" fill="#000000" fill-rule="nonzero"> <path d="M14.25,14.25 L3.75,14.25 L3.75,3.75 L9,3.75 L9,2.25 L3.75,2.25 C2.9175,2.25 2.25,2.925 2.25,3.75 L2.25,14.25 C2.25,15.075 2.9175,15.75 3.75,15.75 L14.25,15.75 C15.075,15.75 15.75,15.075 15.75,14.25 L15.75,9 L14.25,9 L14.25,14.25 Z M10.5,2.25 L10.5,3.75 L13.1925,3.75 L5.82,11.1225 L6.8775,12.18 L14.25,4.8075 L14.25,7.5 L15.75,7.5 L15.75,2.25 L10.5,2.25 Z" id="Shape"></path> </g> </g> </svg>
       </div>
     </div>
     </div>
@@ -121,10 +122,6 @@ export default {
 </script>
 
 <style scoped>
-.content {
-  padding: 16px 0;
-}
-
 .feature:not(:last-child) {
   border-bottom: 1px solid var(--color-grey-50);
 }
@@ -134,18 +131,26 @@ export default {
   margin-top: calc(var(--padding-screen) * -1 + var(--width-button-large) * -1);
 }
 
-.special-properties {
-  padding: 0 var(--padding-screen);;
+.content {
+  padding: 16px 0;
 }
 
-.special-properties h1 {
+.properties {
+  padding: 0 var(--padding-screen);
+}
+
+.property {
+  margin-bottom: 20px;
+}
+
+.properties h1 {
   margin: 0 0 8px;
   font-size: var(--font-size-large);
   font-weight: var(--font-weight-normal);
   font-weight: var(--font-weight-bold);
 }
 
-.special-properties p {
+.properties p {
   margin: 0 0 20px;
 }
 
@@ -153,14 +158,6 @@ export default {
   display: block;
   font-weight: var(--font-weight-bold);
   margin-bottom: 4px;
-}
-
-.properties {
-  padding: 0 var(--padding-screen);;
-}
-
-.property {
-  margin-bottom: 20px;
 }
 
 .table {
@@ -194,11 +191,19 @@ export default {
 .link {
   display: flex;
   align-items: center;
+  border-top: 1px solid var(--color-grey-50);
+  border-bottom: 1px solid var(--color-grey-50);
+  padding: 8px var(--padding-screen);
+  color: var(--color-primary);
+}
+
+.link svg {
+  margin-right: 8px;
 }
 
 .markdown >>> a {
   display: block;
-  color: #4285F4;
+  color: var(--color-primary);
   text-decoration: none;
 }
 
