f93c2e0ea2e83ac33d55c100d47bb1fc9560460f
Bart Jeukendrup <bart@jeukendrup.nl>
2021-06-07 15:57:12 +0200

    fix: wrap div around data table

--- ui/src/components/FeatureTable.vue
+++ ui/src/components/FeatureTable.vue
@@ -34,64 +34,66 @@
         </template>
 
         <template v-slot:default>
-            <span v-if="error">Er is een fout opgetreden tijdens het laden.</span>
-            <span v-if="loading">Bezig met laden...</span>
-            <span v-if="!loading && !error && displayProperties.length === 0"
-                >Geen weergave beschikbaar.</span
-            >
-            <div v-if="!loading && !error && displayProperties.length > 0">
-                <Table class="table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th v-for="property in displayProperties" v-bind:key="property">
-                                    {{ property }}
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="feature in features" v-bind:key="feature.id">
-                                <td>
-                                    <button
-                                        class="iconbutton pin-button"
-                                        v-tippy="{ placement: 'right' }"
-                                        content="Bekijk op kaart"
-                                        aria-label="Bekijk op kaart"
-                                        @click="() => showFeature(feature)"
-                                    >
-                                        <svg
-                                            width="10px"
-                                            height="14px"
-                                            viewBox="0 0 10 14"
-                                            version="1.1"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            xmlns:xlink="http://www.w3.org/1999/xlink"
+            <div>
+                <span v-if="error">Er is een fout opgetreden tijdens het laden.</span>
+                <span v-if="loading">Bezig met laden...</span>
+                <span v-if="!loading && !error && displayProperties.length === 0"
+                    >Geen weergave beschikbaar.</span
+                >
+                <div v-if="!loading && !error && displayProperties.length > 0">
+                    <Table class="table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th v-for="property in displayProperties" v-bind:key="property">
+                                        {{ property }}
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="feature in features" v-bind:key="feature.id">
+                                    <td>
+                                        <button
+                                            class="iconbutton pin-button"
+                                            v-tippy="{ placement: 'right' }"
+                                            content="Bekijk op kaart"
+                                            aria-label="Bekijk op kaart"
+                                            @click="() => showFeature(feature)"
                                         >
-                                            <g
-                                                id="pin"
-                                                stroke="none"
-                                                stroke-width="1"
-                                                fill="none"
-                                                fill-rule="evenodd"
+                                            <svg
+                                                width="10px"
+                                                height="14px"
+                                                viewBox="0 0 10 14"
+                                                version="1.1"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink"
                                             >
-                                                <path
-                                                    d="M5,0 C7.5155,0 9.55,2.0345 9.55,4.55 C9.55,7.9625 5,13 5,13 C5,13 0.45,7.9625 0.45,4.55 C0.45,2.0345 2.4845,0 5,0 Z M5,1.3 C3.206,1.3 1.75,2.756 1.75,4.55 C1.75,6.4025 3.648,9.2365 5,10.972 C6.378,9.2235 8.25,6.422 8.25,4.55 C8.25,2.756 6.794,1.3 5,1.3 Z M5,2.925 C5.89746272,2.925 6.625,3.65253728 6.625,4.55 C6.625,5.44746272 5.89746272,6.175 5,6.175 C4.10253728,6.175 3.375,5.44746272 3.375,4.55 C3.375,3.65253728 4.10253728,2.925 5,2.925 Z"
-                                                    id="Combined-Shape"
-                                                    fill="#000000"
-                                                    fill-rule="nonzero"
-                                                ></path>
-                                            </g>
-                                        </svg>
-                                    </button>
-                                </td>
-                                <td v-for="property in displayProperties" v-bind:key="property">
-                                    {{ feature.properties[property] }}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </Table>
+                                                <g
+                                                    id="pin"
+                                                    stroke="none"
+                                                    stroke-width="1"
+                                                    fill="none"
+                                                    fill-rule="evenodd"
+                                                >
+                                                    <path
+                                                        d="M5,0 C7.5155,0 9.55,2.0345 9.55,4.55 C9.55,7.9625 5,13 5,13 C5,13 0.45,7.9625 0.45,4.55 C0.45,2.0345 2.4845,0 5,0 Z M5,1.3 C3.206,1.3 1.75,2.756 1.75,4.55 C1.75,6.4025 3.648,9.2365 5,10.972 C6.378,9.2235 8.25,6.422 8.25,4.55 C8.25,2.756 6.794,1.3 5,1.3 Z M5,2.925 C5.89746272,2.925 6.625,3.65253728 6.625,4.55 C6.625,5.44746272 5.89746272,6.175 5,6.175 C4.10253728,6.175 3.375,5.44746272 3.375,4.55 C3.375,3.65253728 4.10253728,2.925 5,2.925 Z"
+                                                        id="Combined-Shape"
+                                                        fill="#000000"
+                                                        fill-rule="nonzero"
+                                                    ></path>
+                                                </g>
+                                            </svg>
+                                        </button>
+                                    </td>
+                                    <td v-for="property in displayProperties" v-bind:key="property">
+                                        {{ feature.properties[property] }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </Table>
+                </div>
             </div>
         </template>
     </ExpandButton>
