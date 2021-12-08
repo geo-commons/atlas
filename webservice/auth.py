9a844654999de2f90efa2c4eba17a6b10afaa4c5
Bart Jeukendrup <bart@jeukendrup.nl>
2021-12-08 10:38:40 +0100

    chore: add verify_claims method print statements for debugging

--- webservice/auth.py
+++ webservice/auth.py
@@ -6,19 +6,41 @@ from user_management.models import AtlasUser
 class AtlasOIDCAuthenticationBackend(OIDCAuthenticationBackend):
     def create_user(self, claims):
         user = self.UserModel.objects.create_user(claims.get(settings.OIDC_USERNAME_CLAIM), claims.get('email'))
-        user.first_name = claims.get('name')
+
+        if claims.get('name'):
+            user.first_name = claims.get('name')
+        elif claims.get('sub'):
+            user.first_name = claims.get('sub')
+
         user.save()
+
+        print(claims)
+        print(self.request.session.get('oidc_access_token'))
 
         if settings.OIDC_ACTIVATE_ON_CREATE != 'True':
             user.is_active = False
             user.save()
 
     def update_user(self, user, claims):
-        user.first_name = claims.get('name')
-        user.email = claims.get('email')
+        if claims.get('name'):
+            user.first_name = claims.get('name')
+        elif claims.get('sub'):
+            user.first_name = claims.get('sub')
+
+        if claims.get('email'):
+            user.email = claims.get('email')
+
         user.save()
 
         return user
 
     def filter_users_by_claims(self, claims):
         return AtlasUser.objects.filter(username__iexact=claims.get(settings.OIDC_USERNAME_CLAIM))
+
+    def verify_claims(self, claims):
+        print('Verifying claims')
+
+        print(claims)
+        print(self.request.session.get('oidc_access_token'))
+
+        return True
