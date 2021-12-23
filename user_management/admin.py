e7b3c20d361539cd604404b74677b30fd5bb594d
Bart <bart@jeukendrup.nl>
2021-12-23 12:35:34 +0000

    Mogelijkheid om supergebruiker status te zien in lijstweergave #234

--- user_management/admin.py
+++ user_management/admin.py
@@ -7,7 +7,7 @@ from .models import AtlasUser, AtlasGroup
 
 
 class AtlasUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
