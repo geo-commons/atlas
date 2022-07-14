2f31e858e7c0723ca684c19c44ef073447cb71ec
Bart Jeukendrup <bart@jeukendrup.nl>
2022-07-14 12:40:17 +0200

    feat: reenable authentication for WMS layer

--- ui/src/components/Map/renderers/OpenLayers/components/OlWmsLayer.vue
+++ ui/src/components/Map/renderers/OpenLayers/components/OlWmsLayer.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Projection from 'ol/proj/Projection'
 import TileLayer from 'ol/layer/Tile'
 import TileWMSSource from 'ol/source/TileWMS'
@@ -11,6 +12,20 @@ const rdProjection = new Projection({
     code: 'EPSG:28992',
     units: 'm',
 })
+
+const authenticatedTileLoader = (token) => {
+    return (tile, src) => {
+        const client = new XMLHttpRequest()
+        client.open('GET', src)
+        client.setRequestHeader('Authorization', `Bearer ${token}`)
+
+        client.onload = () => {
+            tile.getImage().src = src
+        }
+
+        client.send()
+    }
+}
 
 export default {
     name: 'WmsLayer',
@@ -26,6 +41,8 @@ export default {
                 tilesOrigin: 117000 + ',' + 498000.00000000023,
             },
             projection: rdProjection,
+            tileLoadFunction:
+                this.loginRequired && this.user ? authenticatedTileLoader(this.user.token) : null,
         })
 
         this.tileLayer = new TileLayer({
@@ -46,6 +63,7 @@ export default {
         url: String,
         layer: String,
         isVisible: Boolean,
+        loginRequired: Boolean,
         opacity: Number,
         zIndex: Number,
         format: String,
@@ -64,6 +82,9 @@ export default {
             this.tileLayer.set('opacity', value)
         },
     },
+    computed: mapState({
+        user: (state) => state.user,
+    }),
 }
 </script>
 
