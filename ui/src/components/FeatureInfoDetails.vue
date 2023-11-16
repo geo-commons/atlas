5b27f6737444b0f87fa2bb4270bfdc51b445d9d2
Yuri <yuri@delta10.nl>
2023-11-16 14:01:04 +0100

    feat: add FeatureInfoDetails.vue

---
+++ ui/src/components/FeatureInfoDetails.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="feature-details-wrapper">
+    <div>
+      <h1>header text feature details</h1>
+    </div>
+
+    <div>
+      <p>dummy content in de body</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeatureInfoDetails",
+  props: {
+    feature: Object,
+    layer: Object,
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.feature-details-wrapper {
+  position: relative;
+  flex-shrink: 0;
+  z-index: 2;
+  margin: var(--padding-screen);
+  padding: 10px 10px;
+  height: 90%;
+  background: var(--color-white);
+  box-shadow: var(--shadow-normal);
+  border-radius: var(--radius-normal);
+}
+</style>
