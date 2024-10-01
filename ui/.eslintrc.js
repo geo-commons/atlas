ff15bb47a8884b4f3e824fdbe49efed77576f3ae
justian <justiandev@gmail.com>
2024-10-01 16:52:58 +0200

    Fix: pipeline

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
     // 'vue/no-unused-vars': 'error'
     "vue/require-default-prop": "off",
     "vue/no-multiple-template-root": "off",
+    "typescript-eslint/no-unused-vars": "warn",
   },
 };
