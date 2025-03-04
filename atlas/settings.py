81a7412ca6286f62c3b228d2df34f2f47c5723f0
georgealpha9 <joris@delta10.nl>
2025-03-04 18:08:18 +0100

    fix(ci): Remove trailing new-lines

--- atlas/settings.py
+++ atlas/settings.py
@@ -362,5 +362,3 @@ if "test" in sys.argv:
     DEBUG = False
     INSTALLED_APPS = [app for app in INSTALLED_APPS if app != "debug_toolbar"]
     MIDDLEWARE = [mw for mw in MIDDLEWARE if mw != "debug_toolbar.middleware.DebugToolbarMiddleware"]
-
-
