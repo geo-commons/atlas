5bfa575291cbc9c2e7bb330efe9d2464f4a9cc33
Yuri <yuri@delta10.nl>
2025-05-12 14:17:30 +0200

    fix: url werkt toch niet lekker

--- ui/src/map.js
+++ ui/src/map.js
@@ -62,41 +62,40 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Then check if the URL settings set any specific settings.
-  layers = layers
-    .map((layer) => {
-      if (layer.is_base) {
-        return {
-          ...layer,
-          is_visible: settings.visibleBase ? settings.visibleBase === layer.id : layer.is_visible,
-        };
-      }
+  layers = layers.map((layer) => {
+    if (layer.is_base) {
+      return {
+        ...layer,
+        is_visible: settings.visibleBase ? settings.visibleBase === layer.id : layer.is_visible,
+      };
+    }
 
-      if (!layer.is_base) {
-        return {
-          ...layer,
-          is_visible: settings.visibleLayers ? settings.visibleLayers.includes(layer.id) : layer.is_visible,
-        };
-      }
-    })
-    .sort((a, b) => {
-      if (!settings.visibleLayers) return 0;
-
-      // Get indices in visibleLayers, defaulting to a high number if not found
-      const indexA = settings.visibleLayers.indexOf(a.id);
-      const indexB = settings.visibleLayers.indexOf(b.id);
-
-      // If both layers are in visibleLayers, sort by their order
-      if (indexA !== -1 && indexB !== -1) {
-        return indexA - indexB;
-      }
-
-      // If only one layer is in visibleLayers, prioritize that one
-      if (indexA !== -1) return -1;
-      if (indexB !== -1) return 1;
-
-      // If neither layer is in visibleLayers, maintain original order
-      return 0;
-    });
+    if (!layer.is_base) {
+      return {
+        ...layer,
+        is_visible: settings.visibleLayers ? settings.visibleLayers.includes(layer.id) : layer.is_visible,
+      };
+    }
+  });
+  // .sort((a, b) => {
+  //   if (!settings.visibleLayers) return 0;
+  //
+  //   // Get indices in visibleLayers, defaulting to a high number if not found
+  //   const indexA = settings.visibleLayers.indexOf(a.id);
+  //   const indexB = settings.visibleLayers.indexOf(b.id);
+  //
+  //   // If both layers are in visibleLayers, sort by their order
+  //   if (indexA !== -1 && indexB !== -1) {
+  //     return indexA - indexB;
+  //   }
+  //
+  //   // If only one layer is in visibleLayers, prioritize that one
+  //   if (indexA !== -1) return -1;
+  //   if (indexB !== -1) return 1;
+  //
+  //   // If neither layer is in visibleLayers, maintain original order
+  //   return 0;
+  // });
 
   const initialState = {
     isEmbed: data.is_embed,
