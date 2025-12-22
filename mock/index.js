1a7c8fba34517165001213112fc9bd559c4f304b
justian <justiandev@gmail.com>
2025-12-22 15:29:34 +0100

    chore: add more demo data

--- mock/index.js
+++ mock/index.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.use("/api", (req, res, next) => {
+server.use("/", (req, res, next) => {
   next();
 });
 
@@ -13,8 +13,12 @@ router.render = (req, res) => {
   const data = res.locals.data;
   const url = new URL(req.originalUrl, "http://localhost");
 
-  // List responses (arrays) => wrap in { results, count, page }
-  if (Array.isArray(data) && req.method === "GET") {
+  const shouldWrapResponse = req.originalUrl.startsWith("/api");
+  const shouldReturnRawResponse = req.originalUrl.startsWith("/raw");
+  const shouldReturnNestedResponse = req.originalUrl.startsWith("/nested");
+
+  if (shouldWrapResponse && Array.isArray(data) && req.method === "GET") {
+    // List responses (arrays) => wrap in { results, count, page }
     const totalCount = res.getHeader("X-Total-Count");
     const page = Number(url.searchParams.get("_page")) || 1;
     const pageSize = Number(url.searchParams.get("_page"))
@@ -29,6 +33,24 @@ router.render = (req, res) => {
       page: page,
       page_size: pageSize,
     });
+  } else if (
+    shouldReturnRawResponse &&
+    Array.isArray(data) &&
+    req.method === "GET"
+  ) {
+    // List responses (arrays) => return raw data
+    res.json(data);
+  } else if (
+    shouldReturnNestedResponse &&
+    Array.isArray(data) &&
+    req.method === "GET"
+  ) {
+    // List responses (arrays) => wrap in { results.data[] }
+    res.json({
+      results: {
+        data: [data],
+      },
+    });
   } else {
     // For single item (GET /api/users/1), POST, PUT, etc.
     res.json(data);
@@ -36,6 +58,8 @@ router.render = (req, res) => {
 };
 
 server.use("/api", router);
+server.use("/nested", router);
+server.use("/raw", router);
 
 const PORT = 4444;
 server.listen(PORT, () => {
