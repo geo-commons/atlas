daf5ee9e2573dd73e08964f5ce2068f6548a897f
justiandev@gmail.com <justiandev@gmail.com>
2026-03-18 13:31:30 +0100

    feat: enhance modify interaction by adding snap functionality

--- ui/src/components/MapRenderer/renderers/OpenLayers/components/OlModifyInteraction.vue
+++ ui/src/components/MapRenderer/renderers/OpenLayers/components/OlModifyInteraction.vue
@@ -5,18 +5,26 @@
 <script setup lang="ts">
 import { inject, onUnmounted, watch } from "vue";
 import type Map from "ol/Map";
-import { Modify } from "ol/interaction";
+import { Modify, Snap } from "ol/interaction";
 import Collection from "ol/Collection";
 import { useEditLayerStore } from "@/stores/edit_layer_store";
 import { EditLayerMode } from "@/types/map";
+import { Feature } from "ol";
+import { Geometry } from "ol/geom";
 
 const map = inject<Map | null>("map", null);
 
 const editLayerStore = useEditLayerStore();
 
 let modify: Modify | null = null;
+let snap: Snap | null = null;
 
 const destroyModifyInteraction = () => {
+  if (map && snap) {
+    map.removeInteraction(snap);
+    snap = null;
+  }
+
   if (map && modify) {
     map.removeInteraction(modify);
     modify = null;
@@ -32,13 +40,24 @@ const syncModifyInteraction = () => {
     return;
   }
 
-  const features = new Collection([editLayerStore.highlightedFeatureAndLayer.feature]);
+  const feature = editLayerStore.highlightedFeatureAndLayer.feature as Feature<Geometry>;
+  const features = new Collection([feature]);
 
   modify = new Modify({
     features: features,
+    // hides the handles of the modify interaction, because they are not needed
+    style: () => undefined,
   });
 
   map.addInteraction(modify);
+
+  snap = new Snap({
+    features: features,
+    edge: true,
+    vertex: true,
+  });
+
+  map.addInteraction(snap);
 };
 
 watch(
